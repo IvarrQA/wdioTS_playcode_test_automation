e9b7ff15ff53734356566e6dae9bb213c0fc5607
NickIT87 <elanir358@gmail.com>
2022-12-06 14:13:38 +0200

    first test aproximation

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,23 @@
 const LoginPage = require('../pageobjects/login.page')
 const SecurePage = require('../pageobjects/secure.page')
 
-describe('My Login application', () => {
+
+// describe('My Login application', () => {
+//     it('should login with valid credentials', async () => {
+//         await LoginPage.open()
+
+//         await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+//         await expect(SecurePage.flashAlert).toBeExisting()
+//         await expect(SecurePage.flashAlert).toHaveTextContaining(
+//             'You logged into a secure area!')
+//     })
+// })
+
+
+describe('login into playcode', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open()
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!')
+        // need to add sleep ... 
     })
 })
-
-
